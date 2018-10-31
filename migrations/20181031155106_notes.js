c47aa1a9d5ae380c26ac56e30dddff55643bf546
Clint Kunz <clintkunz@gmail.com>
2018-10-31 15:51:40 -0600

    notes table

---
+++ migrations/20181031155106_notes.js
@@ -0,0 +1,21 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('notes', table => {
+        table.increments();
+        table
+          .string('title', 128)
+          .notNullable();
+        table
+          .text('content')
+          .notNullable();
+        table
+          .boolean('edited')
+          .defaultTo(0);
+        table
+          .timestamp('timestamp')
+          .defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('notes');
+};
