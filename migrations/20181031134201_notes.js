be7d37e4c711587cad8edc6cb9a330743bc4cf53
Clint Kunz <clintkunz@gmail.com>
2018-10-31 15:09:05 -0600

    migration change

--- migrations/20181031134201_notes.js
+++ migrations/20181031134201_notes.js
@@ -3,7 +3,6 @@ exports.up = function(knex, Promise) {
         table.increments();
         table
           .integer('user_id')
-          .notNullable()
           .references('id')
           .inTable('users');
         table
