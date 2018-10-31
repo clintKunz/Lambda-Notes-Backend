77551d4ed05aedbd07d75f59c9af69af1c5b5e36
Clint Kunz <clintkunz@gmail.com>
2018-10-31 15:16:07 -0600

    table change

--- migrations/20181031134201_notes.js
+++ migrations/20181031134201_notes.js
@@ -2,10 +2,6 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('notes', table => {
         table.increments();
         table
-          .integer('user_id')
-          .references('id')
-          .inTable('users');
-        table
           .string('title', 128)
           .notNullable();
         table
