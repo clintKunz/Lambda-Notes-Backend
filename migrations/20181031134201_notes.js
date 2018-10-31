99f0fe2deb52af075964e0ef0d83079e876aedc9
Clint Kunz <clintkunz@gmail.com>
2018-10-31 13:44:45 -0600

    rearrange migrations for heroku

--- migrations/20181031134201_notes.js
+++ migrations/20181031134201_notes.js
@@ -1,4 +1,3 @@
-
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('notes', table => {
         table.increments();
---
+++ migrations/20181031134231_note_tag.js
@@ -0,0 +1,19 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('note_tag', table => {
+        table.increments();
+        table 
+          .integer('note_id')
+          .notNullable()
+          .references('id')
+          .inTable('notes');
+        table 
+          .integer('tag_id')
+          .notNullable()
+          .references('id')
+          .inTable('tags');
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('note_tag');
+  };
